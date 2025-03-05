d7b5f0c58e05bab1e317f276f5d74813507162b5
Razann6 <rosethamir@gmail.com>
2025-03-05 06:47:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
     const products = document.querySelectorAll(".product");
 
     products.forEach(product => {
+ 
+        const productImage = product.querySelector("img");
+        productImage.style.display = "block"; 
+        productImage.style.visibility = "visible"; 
+
+ 
         const addButton = document.createElement("button");
         addButton.textContent = "Add to Cart";
         addButton.classList.add("add-to-cart");
-  
-        const productImage = product.querySelector("img");
-        
-       
+
+ \
         product.appendChild(addButton);
     });
 });
